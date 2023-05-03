66e242011d99d5af3197d29cd0618683812e3900
Vourone <Vourone@GMail.Com>
2023-05-03 10:38:38 +0700

    -

--- HackerRank/Tutorials/10_Days_JavaScript/Day_2-Conditional_Statements-Switch.js
+++ HackerRank/Tutorials/10_Days_JavaScript/Day_2-Conditional_Statements-Switch.js
@@ -11,13 +11,11 @@ process.stdin.on('data', inputStdin => {
 });
 
 process.stdin.on('end', _ => {
-    inputString = inputString.trim()
-        .split('\n')
-        .map(string => {
-            return string.trim();
-        });
-    
-    main();
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();    
 });
 
 function readLine() {
@@ -28,10 +26,26 @@ function getLetter(s) {
     let letter;
     // Write your code here
     switch (s.charAt(0)) {
-    case ('a' || 'e' || 'o' || 'i' || u):
-        letter = 'A';
-        break;
-    case ('b' || 'c' || 'd' || 'f' || 'g'):
-        …
-    }
+        case ('a' || 'e' || 'o' || 'i' || u):
+            letter = 'A';
+                break;
+                case ('b' || 'c' || 'd' || 'f' || 'g'):
+                    letter = 'B';
+                        break;
+                        case ('h' || 'j' || 'k' || 'l' || 'm'):
+                            letter = 'C';
+                                break;
+                                case ('z' || 'n' || 'p' || 'q' || 'r' || 's' || 't' || 'v' || 'w' || 'x' ||
+                                'y'):
+                                    letter = 'D';
+                                    }
+                                    
     
+    return letter;
+}
+
+function main() {
+    const s = readLine();
+    
+    console.log(getLetter(s));
+}
